8eaf1791c7fbadcf6c408c26a459d4cf13a11a4c
mrpandey1 <pandeynishant12345@gmail.com>
2020-12-03 15:06:29 +0530

    added style to menu-item component

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -42,7 +42,7 @@ class Directory extends React.Component{
             <div className='directory-menu'>  
                 {
                     this.state.sections.map(({title,imageUrl,id,size})=>(
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                        <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}/>
                     ))
                 }
             </div>
