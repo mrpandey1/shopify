2879ef4fb9f653c4903a017c50939ccb1fbd5fbf
mrpandey1 <pandeynishant12345@gmail.com>
2020-12-05 15:43:48 +0530

    storing user data

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -31,6 +31,7 @@ export const createUserProfileDocument = async (userAuth,additionalData)=>{
             console.log('error creating user',error.message);
         }
     }
+    return userRef;
 }
 
 firebase.initializeApp(config);
