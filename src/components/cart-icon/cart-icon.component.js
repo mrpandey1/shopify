649df5ec25eed40b481e14e72a358ca8dd8c559e
mrpandey1 <pandeynishant12345@gmail.com>
2020-12-14 15:08:41 +0530

    fetching the number of items in cart and updating the number on cart icon

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -4,14 +4,21 @@ import './cart-icon.styles.scss';
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden,itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
   });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps=({cart:{cartItems}})=>({
+    itemCount:cartItems.reduce((
+        accumalatedQuantity ,cartItem)=>
+        accumalatedQuantity+cartItem.quantity,
+        0)
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
