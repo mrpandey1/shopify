0583efdeecedcbc7e8022c63d8ef48231451856a
mrpandey1 <pandeynishant12345@gmail.com>
2020-12-14 15:55:15 +0530

    hiding the card dropdown whenever go to checkout button is pressed

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -7,8 +7,9 @@ import CartItem from '../cart-item/cart-item.component';
 import {connect} from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
+import {toggleCartHidden} from '../../redux/cart/cart.actions'
 
-const CartDropdown = ({cartItems,history}) => (
+const CartDropdown = ({cartItems,history,dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -19,7 +20,10 @@ const CartDropdown = ({cartItems,history}) => (
             <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={()=>history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={()=>{
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+            }}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
