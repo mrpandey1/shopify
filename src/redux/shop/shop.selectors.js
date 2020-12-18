c0fe4f6f8d3debe6193669a5bf67ef2dfa47d964
mrpandey1 <pandeynishant12345@gmail.com>
2020-12-18 11:12:19 +0530

    using hash tables instead of arrays for fast traversing

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import {createSelector} from 'reselect';
 
-const COLLECTION_ID_MAP={
-    hats:1,
-    sneakers:2,
-    jackets:3,
-    womens:4,
-    mens:5
-}
 
 const selectShop=state=>state.shop;
 
@@ -18,5 +11,7 @@ export const selectCollections=createSelector(
 export const selectCollection=collectionUrlParam=>
 createSelector(
     [selectCollections],
-    collections=>collections.find(collection=>collection.id===COLLECTION_ID_MAP[collectionUrlParam])
+    collections=>collections.find(
+        collection=>collections[collectionUrlParam]
+        )
 )
