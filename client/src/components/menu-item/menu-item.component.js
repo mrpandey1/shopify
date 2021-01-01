eee8d5ef10ff6d74f778495deb66f940954c574d
mrpandey1 <pandeynishant12345@gmail.com>
2021-01-01 19:59:18 +0530

    added some media queries

--- client/src/components/menu-item/menu-item.component.js
+++ client/src/components/menu-item/menu-item.component.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
-import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle
+} from './menu-item.styles';
 
-const MenuItem=({ title,imageUrl,size,history,linkUrl,match })=>(
-    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
-        <div className='background-image' style={{
-            backgroundImage:`url(${imageUrl})`
-        }}/>
-        <div className='content'>
-            <h1 className='title'>{title}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
-    </div>
-)
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <MenuItemContainer
+    size={size}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <BackgroundImageContainer
+      className='background-image'
+      imageUrl={imageUrl}
+    />
+    <ContentContainer className='content'>
+      <ContentTitle>{title.toUpperCase()}</ContentTitle>
+      <ContentSubtitle>SHOP NOW</ContentSubtitle>
+    </ContentContainer>
+  </MenuItemContainer>
+);
 
 export default withRouter(MenuItem);
