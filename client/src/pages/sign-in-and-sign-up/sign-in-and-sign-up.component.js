6f442711b1f661800db8ea37e9c9b3950cc663d0
mrpandey1 <pandeynishant12345@gmail.com>
2021-01-02 12:27:24 +0530

    made the lauthentication page responsive

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './sign-in-and-sign-up.styles.scss';
-import SignInComponent from'../../components/sign-in/sign-in.component';
-import SignupComponent from '../../components/sign-up/sign-up.component';
-const SignInAndSignUpPage=()=>(
-    <div className='sign-in-and-sign-up'>
-        <SignInComponent/>
-        <SignupComponent/>
-    </div>
-)
+
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
+
+const SignInAndSignUpPage = () => (
+  <SignInAndSignUpContainer>
+    <SignIn />
+    <SignUp />
+  </SignInAndSignUpContainer>
+);
 
 export default SignInAndSignUpPage;
